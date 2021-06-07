b0cb648705f8e861ba3343a5f46f6f6e1b75419f
LightDestory <apb231@gmail.com>
2021-06-07 17:01:49 +0200

    refactor: internal structure update

--- trackbook_server/src/server.ts
+++ trackbook_server/src/server.ts
@@ -1,9 +1,19 @@
 import express, {Application, Request, Response} from 'express';
-import {loginRoute} from './api/loginRoute';
-import {userRoute} from './api/userRoute';
-
+import {loginRoute} from './apis/loginRoute';
+import {userRoute} from './apis/userRoute';
+const configutation = require('dotenv').config();
 const app: Application = express();
-app.use( express.json(), express.text() );
+
+
+function bootstrap() {
+    if(configutation.error){
+        throw configutation.error;
+    }
+    console.log(configutation.parsed);
+}
+
+bootstrap()
+/*app.use( express.json(), express.text() );
 
 app.use('/login', loginRoute);
 
@@ -14,4 +24,5 @@ app.get('/', (req: Request, res: Response) => {
     res.sendStatus(200);
 });
 
-app.listen(5000, () => {console.log("Listening to 5000!")});
+app.listen(process.env.PORT, () => {console.log(`Listening to ${process.env.PORT}!`)});
+*/
