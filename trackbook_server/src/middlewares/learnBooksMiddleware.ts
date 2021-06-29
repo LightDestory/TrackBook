4c781abc668b340f6a734d49351c66be60c8979f
LightDestory <apb231@gmail.com>
2021-06-29 21:48:56 +0200

    refactor: JsonObject for userDataBackup

--- trackbook_server/src/middlewares/learnBooksMiddleware.ts
+++ trackbook_server/src/middlewares/learnBooksMiddleware.ts
@@ -1,14 +1,18 @@
 import {NextFunction, Request, Response} from "express";
 import Knex from "knex";
 import db from "../db/database"
+import {Sender} from "../utils/Sender";
 
 export function learnBooks(req: Request, res: Response, next: NextFunction) {
-    if(req.method == "GET" || (Object.keys(req.body).length == 0)) {
+    if(req.method == "POST" && (Object.keys(req.body).length == 0)) {
+        Sender.getInstance().sendError(res, Sender.ERROR_TYPE_PARAMETER);
+        return;
+    } else if(req.method == "GET" || (Object.keys(req.body.books).length == 0)) {
         next();
     } else {
         // @ts-ignore
         let con: Knex = db.getConnection();
-        let data = req.body;
+        let data = req.body.books;
         con('books')
             .select("isbn")
             .then((rows : any[]) => {
