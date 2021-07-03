9ff1c14dbe5b3fa09f110a004e9de7650a5c1d2d
LightDestory <apb231@gmail.com>
2021-07-03 12:10:47 +0200

    fix: resources link error

---
+++ trackbook_app/app/src/main/java/com/lightdestory/trackbook/BookshelfActivity.kt
@@ -0,0 +1,11 @@
+package com.lightdestory.trackbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookshelfActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.bookshelf)
+    }
+}
