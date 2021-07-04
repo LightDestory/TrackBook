4580e52a5bb68a23e85c879c6da1ff74cba9e98a
LightDestory <apb231@gmail.com>
2021-07-04 13:46:36 +0200

    forgot kotlin class

--- trackbook_app/app/src/main/java/com/lightdestory/trackbook/AboutActivity.kt
+++ trackbook_app/app/src/main/java/com/lightdestory/trackbook/AboutActivity.kt
@@ -1,5 +1,7 @@
 package com.lightdestory.trackbook
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lightdestory.trackbook.databinding.AboutBinding
@@ -13,7 +15,13 @@ class AboutActivity : AppCompatActivity() {
         binding.include.toolbar.navigationIcon = getDrawable(R.drawable.icon_back)
         binding.include.toolbar.setNavigationOnClickListener { onBackPressed() }
         binding.include.toolbarTitle.text = getString(R.string.label_About)
+        binding.aboutGitHubCard.setOnClickListener { visitRepository() }
         setContentView(binding.root)
+    }
+
+    private fun visitRepository(){
+        val goRepo: Intent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.about_GitHubLink)))
+        startActivity(goRepo)
     }
 
     override fun onBackPressed() {
